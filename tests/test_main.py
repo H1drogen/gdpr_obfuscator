6b9f56c4eeb95fb389430664518f0a4209acc8a1
SimonH <professional.simonhossain@gmail.com>
2025-03-01 14:52:34 +0000

    more unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,20 +15,29 @@ def test_s3_setup():
         bucket_name = 'my_ingestion_bucket'
         s3.create_bucket(Bucket=bucket_name)
 
-        csv_data = """student_id,name,course,cohort,graduation_date,email_address
-        1234,John Smith,Software,2024-03-31,j.smith@email.com"""
+        csv_data = """student_id,name,course,graduation_date,email_address
+        1234,John Smith,Software,2024-03-31,j.smith@email.com
+        5678,Jane Doe,Data Science,2023-06-30"""
         s3.put_object(Bucket=bucket_name, Key='new_data/file1.csv', Body=csv_data)
         yield s3
 
-def test_lambda_handler_obfuscates_pii_fields(test_s3_setup):
+def test_lambda_handler_creates_new_obfuscated_file(test_s3_setup):
     event = {
         'file_to_obfuscate': 's3://my_ingestion_bucket/new_data/file1.csv',
         'pii_fields': ['name', 'email_address']
     }
 
-    context = {}
+    response = lambda_handler(event, {})
 
-    response = lambda_handler(event, context)
+    assert test_s3_setup.get_object(Bucket='my_ingestion_bucket', Key='new_data/file1_obfuscated.csv')['Body'].read()
+
+def test_lambda_handler_obfuscates_relevant_pii_field(test_s3_setup):
+    event = {
+        'file_to_obfuscate': 's3://my_ingestion_bucket/new_data/file1.csv',
+        'pii_fields': ['name', 'email_address']
+    }
+
+    response = lambda_handler(event, {})
 
     assert response['statusCode'] == 200
     assert 'File obfuscated and saved to s3://my_ingestion_bucket/new_data/file1_obfuscated.csv' in response['body']
@@ -38,6 +47,27 @@ def test_lambda_handler_obfuscates_pii_fields(test_s3_setup):
 
     assert df_obfuscated['name'].iloc[0] == '***'
     assert df_obfuscated['email_address'].iloc[0] == '***'
+    assert df_obfuscated['student_id'].iloc[0] == 1234
+
+def test_lambda_handler_obfuscates_all_pii_fields(test_s3_setup):
+    event = {
+        'file_to_obfuscate': 's3://my_ingestion_bucket/new_data/file1.csv',
+        'pii_fields': ['name', 'email_address']
+    }
+
+    response = lambda_handler(event, {})
+
+    assert response['statusCode'] == 200
+    assert 'File obfuscated and saved to s3://my_ingestion_bucket/new_data/file1_obfuscated.csv' in response['body']
+
+    obfuscated_obj = test_s3_setup.get_object(Bucket='my_ingestion_bucket', Key='new_data/file1_obfuscated.csv')['Body'].read()
+    df_obfuscated = pd.read_csv(BytesIO(obfuscated_obj))
+
+    assert df_obfuscated['name'].iloc[0] == '***'
+    assert df_obfuscated['email_address'].iloc[0] == '***'
+    assert df_obfuscated['name'].iloc[1] == '***'
+    assert df_obfuscated['email_address'].iloc[1] == '***'
+
 
 def test_lambda_handler_handles_empty_pii_fields(test_s3_setup):
     event = {
